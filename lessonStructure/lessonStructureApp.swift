9b3604613232db82afa740d8531ace8a1172ea57
lukasg8 <lukasg@uchicago.edu>
2023-08-10 18:07:37 +0200

    Initial Commit

---
+++ lessonStructure/lessonStructureApp.swift
@@ -0,0 +1,17 @@
+//
+//  lessonStructureApp.swift
+//  lessonStructure
+//
+//  Created by Lukas on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct lessonStructureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
